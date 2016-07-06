8cb92b13ee97c96cb4d1b54cbe33da0272793043
sunho0301 <sunho4572@gmail.com>
2016-07-06 08:05:03 -0700

    1

--- js/movies.js
+++ js/movies.js
@@ -3,3 +3,47 @@
 //note that _MOVIES has been loaded as a global
 
 /* Your code goes here */
+
+var myApp = angular.module('MoviesApp', []);
+
+// this has to be done before controller
+var initCallBack = function($scope) {
+    
+    // make a model that is my name
+    $scope.myName = 'Sunho';
+
+    // make a model that is a message
+    $scope.message = "Hello world!";
+
+    $scope.showDemo = true;
+
+    // movie model
+    $scope.movies = [
+     {title:'Gone with the Wind', year:1939},
+     {title:'Casablanca', year:1942},
+     {title:'Star Wars', year:1979}
+    ];
+
+    //console.log($scope.movies[0].year); // =>1939
+
+    $scope.sayHello = function() {
+        console.log("Hello World!");
+    };
+
+    $scope.reset = function() {
+        $scope.message = '';
+    }
+};
+
+//                             service we use/ constructor(callback function)
+myApp.controller('MoviesCtrl', ['$scope', initCallBack]);
+
+
+/* shortened version
+// better!
+
+myApp.controller('MoviesCtrl', ['$scope', function($scope) {
+    //controller initialized
+}]);
+
+*/
--- js/warmup.js
+++ js/warmup.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//$(document).ready(function() {
 //Use jQuery and the 'keyup' event to say "good morning" to whoever's name 
 //is typed into the #toGreet input. 
 // - The greeting should dynamically change as you type!
@@ -7,3 +7,31 @@
 
 //Don't forget to wrap your script in a callback so it runs when the document
 //has loaded.
+
+/* try1.... not working
+$('text').type(function(event) {
+    var name = encodeURIComponent(document.querySelector('input').value);
+    var greet = function(name) {
+        var elementP = document.createElement("p");
+        var sentence = document.createTextNode("Hello, "+name);
+        elementP.appendChild(sentence);
+        var site = document.getElementById('greeting');
+        site.appendChild(elementP);
+
+    }
+});*/
+
+// answer
+// when keyup event occurs
+// 'input' or '#toGreet'
+$('input').keyup(function(){
+    var name = $(this).val() // == $('input').val()
+    $('#greeting').text('Hello, '+name);
+})
+
+
+
+
+
+
+//});
